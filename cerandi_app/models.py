753468f7a917f52d43a9613f6f7629fef4d99271
Christopher Helm <training.helm@gmail.com>
2015-11-17 09:33:01 +0100

    test comments to classes to code

--- cerandi_app/models.py
+++ cerandi_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+# class financial client
+
+
+# class financial advisor
+
+# class bank of financial advisor
+    # link to financial adivsor
+
+# class invenstment table: link client to selected stocks
