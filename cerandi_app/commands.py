13dc3d7bb57df988ec0c80025a95f9644445a3c6
cvd <christopher.vandun@gmail.com>
2015-11-17 12:41:24 +0100

    SAP integration (watchlists and users creatable), no optimization yet)

--- cerandi_app/commands.py
+++ cerandi_app/commands.py
@@ -4,9 +4,11 @@ import requests
 import json
 
 
-def create_user():
+def create_user(username):
+    email = "christopher.ott@gmx.net"
+    post_data = '{"attributes": [{"label": "email","value": "'+ email +'"},{"label": "user name","value": "'+ username +'"}]}'
     response = requests.post('https://sentinel.apimanagement.hana.ondemand.com:443/api/watchlist_service/rest/users',
-                         data={"username": "abcd"},
+                         data=post_data,
                          allow_redirects=True,
                          headers={'api_key': 'Jds7JaVbE3alY4cVeEwWu2MRhLxWXKsG'})
     assert response.status_code < 300
@@ -38,17 +40,40 @@ def get_stock_facts(stock_id):
         return stock_topics
 
 
-def add_watchlist():
-    response = requests.post('https://sentinel.apimanagement.hana.ondemand.com:443/api/watchlist_service/rest/users/564A5B8DF5F2C576E1000000AC12017B/watchlists',
+######## params ########
+#ids:    list of Strings
+def create_watchlist(user_id, stock_ids):
+    post_data = '{"type": "compose","watchlist_name": "reference compose","preferences": ' \
+                '{' \
+                    '"exchange_id": "1",' \
+                    '"transaction_cost": 0.25,' \
+                    '"selection_metric": "returns",' \
+                    '"strategy_type": "single",' \
+                    '"backtesting_duration_in_months": 6,' \
+                    '"is_managed": false, ' \
+                    '"stocks": ' + str(stock_ids) + \
+                    '}' \
+                '}'
+    response = requests.post('https://sentinel.apimanagement.hana.ondemand.com:443/api/watchlist_service/rest/users/'+ user_id +'/watchlists',
+                             data=post_data,
                              allow_redirects=True,
                              headers={'api_key': 'Jds7JaVbE3alY4cVeEwWu2MRhLxWXKsG'})
     response_json = json.loads(response.text)
+    print response_json
     watchlist_id = 5 #TODO: response_json["some_label"]
     return watchlist_id
 
 
 def main():
-    get_stock_facts("26")
+    #a = create_user("a", "b")
+    #b = create_user("c", "b")
+    #c = create_user("ar", "b")
+    #print a
+    #print b
+    #print c
+    user_id = create_user("bla")
+    print user_id
+    create_watchlist(user_id, ["12", "13"])
 
 if __name__ == "__main__":
     main()
